bd34e4c1645920f57dd983396404bc66336c97d1
Tomáš Vladík <tomas.vladik@solarwinds.com>
2023-10-17 21:48:56 +0200

    Initial commit

---
+++ reproIssuesOdbc-247/MyQueryRunner.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Odbc;
+using System.Diagnostics;
+using System.Linq;
+
+namespace OdbcTest
+{
+    public class MyQueryRunner
+    {
+
+        public DataTable[] ExecuteQuery(string connectionString, string query, TimeSpan queryTimeout)
+        {
+            var stopwatch = Stopwatch.StartNew();
+            var totalTimeout = (int) Math.Ceiling(queryTimeout.TotalSeconds)  * 1000;
+
+            using (var connection = new OdbcConnection(connectionString))
+            {
+                connection.ConnectionTimeout = (int) Math.Ceiling(queryTimeout.TotalSeconds);
+                connection.Open();
+
+                if (stopwatch.ElapsedMilliseconds >= totalTimeout)
+                {
+                    throw new TimeoutException();
+                }
+
+                return LoadData(query, connection, (int) Math.Ceiling(queryTimeout.TotalSeconds), stopwatch, totalTimeout).ToArray();
+            }
+        }
+
+        private IEnumerable<DataTable> LoadData(string query, OdbcConnection connection, int roundedQueryTimeout, Stopwatch stopwatch, int totalTimeout)
+        {
+            using (var command = new OdbcCommand(query, connection))
+            {
+                command.CommandTimeout = roundedQueryTimeout;
+
+                using (var reader = command.ExecuteReader())
+                {
+                    do
+                    {
+                        var dataTable = new DataTable();
+                        if (reader.HasRows)
+                        {
+                            var schemaTable = reader.GetSchemaTable();
+                            // process schema
+                            while (reader.Read())
+                            {
+                                var newRow = dataTable.Rows.Add();
+                                //process columns ...
+                            }
+                        }
+
+                        yield return dataTable;
+                    }
+                    while (reader.NextResult());
+                }
+            }
+        }
+    }
+}
---
+++ reproIssuesOdbc-247/MyQueryRunnerTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NUnit.Framework;
+using OdbcTest;
+
+namespace DatabaseWorkerTests
+{
+    [TestFixture]
+    public class MyQueryRunnerTests
+    {
+        private const string Query = @"
+SELECT 1;
+SELECT 1;
+";
+
+
+
+        [Test]
+        public void Execute_AgainstRealDb()
+        {
+            
+            string dbServerAddress="127.0.0.1"; //change to match your environment
+            string dbRootPassword="Supercalifragilisticexpialidocious"; //change to match your environment
+
+
+            var executor = new MyQueryRunner();
+            executor.ExecuteQuery(
+                $"Driver={{MariaDB ODBC 3.1 Driver}};Server={dbServerAddress};User=root;Password={dbRootPassword};Option=3;Port=3306",
+                Query
+                , TimeSpan.FromSeconds(30));
+        }
+
+    }
+}
